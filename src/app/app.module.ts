978a655e42562375a4e83d11304232bfd7b619e9
thewaterfall <floodlight98@gmail.com>
2019-10-27 13:34:25 +0200

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { BoardspaceComponent } from './components/boardspace/boardspace.component';
 import { BoardComponent } from './components/board/board.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MaterialModule} from './modules/material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     BoardspaceComponent,
-    BoardComponent
+    BoardComponent,
+    HeaderComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material';
 
 let components = [
-
+  MatToolbarModule
 ];
 
 @NgModule({
