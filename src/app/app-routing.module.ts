4614cbf353c2eb223cc38a2f21e138111f141212
thewaterfall <floodlight98@gmail.com>
2019-10-27 13:20:11 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {BoardspaceComponent} from './components/boardspace/boardspace.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'boardspace'},
+  {path: 'login', component: LoginComponent},
+  {path: 'boardspace', component: BoardspaceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
