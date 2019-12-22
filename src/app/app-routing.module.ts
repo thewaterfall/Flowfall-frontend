b25262548915c4becab76f54ed36560bdab10c95
thewaterfall <floodlight98@gmail.com>
2019-12-22 19:33:27 +0200

    Add new routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from './components/login/login.component';
 import {BoardspaceComponent} from './components/boardspace/boardspace.component';
 import {AuthGuard} from './auth/guards/auth.guard';
 import {LoginGuard} from './auth/guards/login.guard';
+import {BoardComponent} from './components/board/board.component';
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'boardspace'},
   {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
-  {path: 'boardspace', component: BoardspaceComponent, canActivate: [AuthGuard]}
+  {path: 'boardspace', component: BoardspaceComponent, canActivate: [AuthGuard]},
+  {path: 'b/:id', component: BoardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
