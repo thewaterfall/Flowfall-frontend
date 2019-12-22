ad05a361c7cd75ee4b844c9ac39fee2e4c32a48b
thewaterfall <floodlight98@gmail.com>
2019-12-22 19:30:57 +0200

    Add board service

---
+++ src/app/services/board.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Board} from '../models/Board';
+import {environment} from '../../environments/environment';
+
+@Injectable()
+export class BoardService {
+  BASE_URL = `${environment.api_url}/boards`;
+
+  constructor(private http: HttpClient) {}
+
+  getBoardsByUserId(id: string): Observable<Board[]> {
+    return this.http.get<Board[]>(`${this.BASE_URL}/u/${id}`);
+  }
+
+  getBoardById(id: string): Observable<Board> {
+    return this.http.get<Board>(`${this.BASE_URL}/${id}`);
+  }
+
+  updateBoard(board: Board): Observable<any> {
+    return this.http.put<Observable<any>>(this.BASE_URL, board);
+  }
+
+}
