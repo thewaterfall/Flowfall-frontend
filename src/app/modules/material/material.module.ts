bc8913239bdde72a96c847f1db96f46371849827
thewaterfall <floodlight98@gmail.com>
2019-12-22 19:32:48 +0200

    Add a few material modules

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatDialogModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 let components = [
   MatToolbarModule,
   MatInputModule,
-  MatButtonModule
+  MatButtonModule,
+  DragDropModule,
+  MatDialogModule,
+  MatButtonModule,
+  MatInputModule,
 ];
 
 @NgModule({
