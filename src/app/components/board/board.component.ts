e73f944b371016837d1414094c9be734e76fb462
thewaterfall <floodlight98@gmail.com>
2019-12-22 19:33:10 +0200

    Update components

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Board} from '../../models/Board';
+import {BoardService} from '../../services/board.service';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {BoardColumn} from '../../models/BoardColumn';
+import {Row} from '../../models/Row';
+import {MatDialog} from '@angular/material';
+import {AddRowDialogComponent} from '../dialogs/add-row-dialog/add-row-dialog.component';
+import {AddColumnDialogComponent} from '../dialogs/add-column-dialog/add-column-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -6,10 +15,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  boardId: string;
+  currentBoard: Board = new Board();
+  connectedList: string[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private boardService: BoardService, private dialog: MatDialog) {
+    this.route.params.subscribe(
+      params => {
+        this.boardId = params['id'];
+
+        this.boardService.getBoardById(this.boardId).subscribe(
+          data => {
+            console.log(data);
+            this.currentBoard = data;
+            this.fillConnectedList(data.boardColumns);
+          },
+          error => console.log(error)
+        );
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  dropRow(event: CdkDragDrop<Row[]>) {
+    if (event.previousContainer === event.container) {
+      if (event.previousIndex === event.currentIndex)
+        return;
+
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+
+    this.recalculateIndices(this.currentBoard);
+    this.boardService.updateBoard(this.currentBoard).subscribe();
+  }
+
+  dropColumn(event: CdkDragDrop<BoardColumn[]>) {
+    if (event.previousIndex === event.currentIndex)
+      return;
+
+    moveItemInArray(this.currentBoard.boardColumns, event.previousIndex, event.currentIndex);
+
+    this.recalculateIndices(this.currentBoard);
+    this.boardService.updateBoard(this.currentBoard).subscribe();
+  }
+
+  fillConnectedList(columns: BoardColumn[]) {
+    columns.forEach(data => {
+      this.connectedList.push(data.name);
+    });
+  }
+
+  recalculateIndices(board: Board) {
+    for (let i = 0; i < board.boardColumns.length; i++) {
+      board.boardColumns[i].index = i + 1;
+      for (let j = 0; j < board.boardColumns[i].rows.length; j++) {
+        board.boardColumns[i].rows[j].index = j + 1;
+      }
+    }
+  }
+
+  addColumn() {
+    const dialogRef = this.dialog.open(AddColumnDialogComponent, {
+      width: '250px'
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      if (data !== undefined) {
+        this.currentBoard.boardColumns.push(data);
+        this.recalculateIndices(this.currentBoard);
+        this.boardService.updateBoard(this.currentBoard).subscribe();
+      }
+    });
+  }
+
+  addRow(column: BoardColumn) {
+    const dialogRef = this.dialog.open(AddRowDialogComponent, {
+      width: '250px'
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      if (data !== undefined) {
+        column.rows.push(data);
+        this.recalculateIndices(this.currentBoard);
+        this.boardService.updateBoard(this.currentBoard).subscribe();
+      }
+    });
+  }
+
+  // TODO: implement
+  deleteRow(column: BoardColumn, row: Row) {
+    column.rows = column.rows.filter(colRow => colRow !== row);
   }
 
 }
--- src/app/components/boardspace/boardspace.component.ts
+++ src/app/components/boardspace/boardspace.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {BoardService} from '../../services/board.service';
+import {TokenStorageService} from '../../auth/services/token-storage.service';
+import {Board} from '../../models/Board';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-boardspace',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardspaceComponent implements OnInit {
 
-  constructor() { }
+  boards: Board[] = [];
+
+  constructor(private boardService: BoardService, private tokenStorage: TokenStorageService,
+              private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.boardService.getBoardsByUserId(this.tokenStorage.getId()).subscribe(
+      data => {
+        console.log(data);
+        this.boards = data;
+      },
+      error => console.log(error)
+    );
+  }
+
+  navigateToBoard(board: Board) {
+    this.router.navigate([`/b`, board.id]);
   }
 
 }
