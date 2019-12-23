2cf61031b801ed557413df38c590975a0223528f
thewaterfall <floodlight98@gmail.com>
2019-12-23 09:11:06 +0200

    Add material import

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatDialogModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 
 let components = [
@@ -10,6 +10,7 @@ let components = [
   MatDialogModule,
   MatButtonModule,
   MatInputModule,
+  MatCardModule
 ];
 
 @NgModule({
