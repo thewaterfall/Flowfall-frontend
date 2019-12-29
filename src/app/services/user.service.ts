b0c6fb5b1fa6c1ba3caeeb51b5171235ab814dfd
thewaterfall <floodlight98@gmail.com>
2019-12-29 11:50:49 +0200

    Add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/User';
+
+@Injectable()
+export class UserService {
+
+  private BASE_URL = `${environment.api_url}/users`;
+
+  constructor(private http: HttpClient) { }
+
+  getCollaboratorsByBoardId(boardId: number): Observable<User[]> {
+    return this.http.get<User[]>(`${this.BASE_URL}/b/${boardId}/collab`);
+  }
+}
