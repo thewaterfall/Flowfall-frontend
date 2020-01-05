a7c87b3de80656e89add749e5d5484555559ae5d
thewaterfall <floodlight98@gmail.com>
2020-01-05 18:10:40 +0200

    Add styles for login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../auth/services/auth.service';
 import {User} from '../../models/User';
 import {TokenStorageService} from '../../auth/services/token-storage.service';
+import {environment} from '../../../environments/environment';
+import {GOOGLE_PROVIDER, REDIRECT_URI} from '../../constants/OAuth2Constants';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +13,11 @@ import {TokenStorageService} from '../../auth/services/token-storage.service';
 export class LoginComponent implements OnInit {
 
   private user: User;
+  private GOOGLE_PROVIDER_URL = `${environment.oauth2_url}?provider=${GOOGLE_PROVIDER}&redirect_uri=${REDIRECT_URI}`;
 
   constructor(private authService: AuthService, private tokenStorage: TokenStorageService) {
     this.user = new User();
+
   }
 
   ngOnInit() {
