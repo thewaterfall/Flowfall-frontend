c79b4513f277f49abe0541578667a3742f3e1076
thewaterfall <floodlight98@gmail.com>
2020-01-06 18:28:37 +0200

    Move from sessionStorage to localStorage

--- src/app/auth/services/token-storage.service.ts
+++ src/app/auth/services/token-storage.service.ts
@@ -16,7 +16,7 @@ export class TokenStorageService {
   constructor() { }
 
   clear() {
-    window.sessionStorage.clear();
+    window.localStorage.clear();
   }
 
   saveData(data: JwtResponse) {
@@ -41,41 +41,41 @@ export class TokenStorageService {
   }
 
   saveToken(token: string) {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   getToken(): string {
-    return window.sessionStorage.getItem(TOKEN_KEY);
+    return window.localStorage.getItem(TOKEN_KEY);
   }
 
   saveEmail(email: string) {
-    window.sessionStorage.removeItem(EMAIL_KEY);
-    window.sessionStorage.setItem(EMAIL_KEY, email);
+    window.localStorage.removeItem(EMAIL_KEY);
+    window.localStorage.setItem(EMAIL_KEY, email);
   }
 
   getEmail(): string {
-    return window.sessionStorage.getItem(EMAIL_KEY);
+    return window.localStorage.getItem(EMAIL_KEY);
   }
 
   saveId(id: string) {
-    window.sessionStorage.removeItem(ID_KEY);
-    window.sessionStorage.setItem(ID_KEY, id);
+    window.localStorage.removeItem(ID_KEY);
+    window.localStorage.setItem(ID_KEY, id);
   }
 
   getId() {
-    return window.sessionStorage.getItem(ID_KEY);
+    return window.localStorage.getItem(ID_KEY);
   }
 
   saveAuthorities(authorities: string[]) {
-    window.sessionStorage.removeItem(AUTHORITIES_KEY);
-    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+    window.localStorage.removeItem(AUTHORITIES_KEY);
+    window.localStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
   }
 
   getAuthorities(): Role[] {
     let roles: Role[] = [];
 
-    JSON.parse(window.sessionStorage.getItem(AUTHORITIES_KEY)).foreach(authority => {
+    JSON.parse(window.localStorage.getItem(AUTHORITIES_KEY)).foreach(authority => {
       roles.push(new Role(authority));
     });
 
