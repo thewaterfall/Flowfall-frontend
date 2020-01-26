cd062415b25c49c1f61d3fa93c49ba52dcabfe4b
thewaterfall <floodlight98@gmail.com>
2020-01-26 23:08:00 +0200

    Add methods into row message service

--- src/app/services/row-message.service.ts
+++ src/app/services/row-message.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {RowMessage} from '../models/RowMessage';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class RowMessageService {
 
-  constructor() { }
+  BASE_URL = `${environment.api_url}/rowMessages`;
+
+  constructor(private http: HttpClient) { }
+
+  getRowMessagesByRowId(rowId: number): Observable<RowMessage[]> {
+    return this.http.get<RowMessage[]>(`${this.BASE_URL}/r/${rowId}`);
+  }
+
+  deleteRowMessage(id: number): Observable<any> {
+    return this.http.delete(`${this.BASE_URL}/${id}`);
+  }
 }
